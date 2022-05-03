d0e34550f5ce1a4f1e77d41f09326bd389c2fcd0
Simmo Saan <simmo.saan@gmail.com>
2022-05-03 17:39:21 +0300

    Stuff

--- src/analyses/libraryDsl.ml
+++ src/analyses/libraryDsl.ml
@@ -34,9 +34,19 @@ type ('k, 'r) arg_desc = {
   capture: (Cil.exp, 'k, 'r) Pat.t;
 }
 
+type ('k, 'r) arg_desc' =
+  | []: ('r, 'r) arg_desc'
+  | (::): ('k, 'm) arg_desc * ('m, 'r) arg_desc' -> ('k, 'r) arg_desc'
+
+
 type ('k, 'r) args_desc =
   | []: ('r, 'r) args_desc
-  | (::): ('k, 'm) arg_desc * ('m, 'r) args_desc -> ('k, 'r) args_desc
+  | (::): ('k, 'm) arg_desc' * ('m, 'r) args_desc -> ('k, 'r) args_desc
+
+let (__) = {
+  accesses = [];
+  capture = Pat.ignore;
+}
 
 let r c = {
   accesses = [`Read];
@@ -47,9 +57,17 @@ let rw c = {
   capture = c
 }
 
+let rec special': type k r. (k, r) arg_desc' -> (Cil.exp, k, r) Pat.t = function
+  | [] -> fun x k -> k
+  (* | arg :: args -> Pat.(^::) arg.capture (special' args) *)
+  | arg :: args -> fun x k ->
+    let k = arg.capture x k in
+    let k = special' args x k in
+    k
+
 let rec special: type k r. (k, r) args_desc -> (Cil.exp list, k, r) Pat.t = function
   | [] -> Pat.nil
-  | arg :: args -> Pat.(^::) arg.capture (special args)
+  | arg :: args -> Pat.(^::) (special' arg) (special args)
 
 type special = [
   | `Lock of Cil.exp
@@ -58,16 +76,22 @@ type special = [
 
 type accs = Cil.exp list -> (access * Cil.exp list) list
 
+let rec accs': type k r. (k, r) arg_desc' -> access list = function
+  | [] -> []
+  | arg_desc' :: arg_desc'' ->
+    let accs' = accs' arg_desc'' in
+    arg_desc'.accesses @ accs'
+
 let rec accs: type k r. (k, r) args_desc -> accs = fun args_desc args ->
   match args_desc, args with
   | [], [] -> []
   | arg_desc :: args_desc, arg :: args ->
-    let accs' = accs args_desc args in
+    let accs'' = accs args_desc args in
     List.fold_left (fun (accs'': (access * Cil.exp list) list) (acc: access) ->
         match List.assoc_opt acc accs'' with
         | Some args -> (acc, arg :: args) :: List.remove_assoc acc accs''
         | None -> (acc, arg :: args) :: accs''
-      ) accs' arg_desc.accesses
+      ) accs'' (accs' arg_desc)
   | _, _ -> invalid_arg "accs"
 
 type desc = {
@@ -81,6 +105,6 @@ let (>>) p f = {
 }
 
 (* let p = [r Pat.arg; rw Pat.ignore; rw Pat.arg] >> fun e1 r2 -> `Lock e1 *)
-let p = [r Pat.ignore; rw Pat.ignore; rw Pat.ignore] >> `Unknown
+let p = [r Pat.ignore; __; rw Pat.ignore] >> `Unknown
 let s = p.special
 let a = p.accs
