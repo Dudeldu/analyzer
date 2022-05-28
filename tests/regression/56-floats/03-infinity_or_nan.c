7833413e82cb166599252c19979dbd086e9efcd6
Dominik Berger <dominik.berger@protonmail.com>
2022-05-28 14:24:14 +0200

    Fix regression test naming and merge them together

---
+++ tests/regression/56-floats/03-infinity_or_nan.c
@@ -0,0 +1,11 @@
+// PARAM: --enable ana.float.interval
+#include <stdio.h>
+
+void main()
+{
+    double data;
+    fscanf(stdin, "%lf", &data);
+    
+    double result1 = data + 1.0; // WARN: Could be +/-infinity or Nan
+    double result2 = data / 0.; // WARN: Could be +/-infinity or Nan
+}
