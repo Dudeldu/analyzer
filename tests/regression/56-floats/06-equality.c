27f8267f8e2e376ef236bfcc9581ae272cb2767e
Dominik Berger <dominik.berger@protonmail.com>
2022-06-16 11:53:22 +0200

    In 06-equality.c, remove DBL_MAX and work with a smaller double value

--- tests/regression/56-floats/06-equality.c
+++ tests/regression/56-floats/06-equality.c
@@ -5,10 +5,7 @@ void main()
     int check1 = (0.2 == 0.2); // WARN
     int check2 = (0.2 != 0.3); // WARN
 
-    // The following is __DBL_MAX__
-    double my_max = 179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858368.000000;
-
-    // As numbers this big cannot be represented exactly anymore by double, this "- 1" will get
-    // completely consumed.
-    double max_minus_one = my_max - 1; // WARN
+    // Not all integers that are this big are representable in doubles anymore...
+    double high_value = 179769313486231568384.0;
+    double x = high_value + 1; // WARN
 }
