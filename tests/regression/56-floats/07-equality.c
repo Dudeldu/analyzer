d2136864afa5bb62115a0409911f66531a7eab55
Dudeldu <mustermann.informatik@gmail.com>
2022-06-17 09:25:29 +0200

    Change values in additional warnings test to use DBL_MAX

--- tests/regression/56-floats/07-equality.c
+++ tests/regression/56-floats/07-equality.c
@@ -1,4 +1,5 @@
 // PARAM: --enable ana.float.interval --enable warn.float
+#include <float.h>
 
 void main()
 {
@@ -8,4 +9,10 @@ void main()
     // Not all integers that are this big are representable in doubles anymore...
     double high_value = 179769313486231568384.0;
     double x = high_value + 1; // WARN
+
+    double a = DBL_MAX; // NOWARN
+    double b = a - 1;   // WARN
+
+    double e = (double)(1.7976931348623157e+308L); // NOWARN
+    double f = e - 1;                              // WARN
 }
