ea6885ca4c43ca0814e938a7667e372f13848bef
Dudeldu <mustermann.informatik@gmail.com>
2022-06-19 11:18:07 +0200

    Fix invariant computation according to IEE754

--- tests/regression/57-floats/05-invariant.c
+++ tests/regression/57-floats/05-invariant.c
@@ -56,38 +56,38 @@ int main()
 
     if (a + 5.f < 10.f)
     {
-        assert(a < 5.); // SUCCESS!
+        assert(a <= 5.); // SUCCESS!
     }
     if (b + 5.f < 10.f)
     {
-        assert(b < 5.f); // SUCCESS!
+        assert(b <= 5.f); // SUCCESS!
     }
 
     if (a * 2. < 6.f)
     {
-        assert(a < 3.); // SUCCESS!
+        assert(a <= 3.); // SUCCESS!
     }
     if (b * 2.f < 6.f)
     {
-        assert(b < 3.f); // SUCCESS!
+        assert(b <= 3.f); // SUCCESS!
     }
 
     if (a / 3. > 10.)
     {
-        assert(a > 30.); // SUCCESS!
+        assert(a >= 30.); // SUCCESS!
     }
     if (b / 3.f > 10.f)
     {
-        assert(b > 30.); // SUCCESS!
+        assert(b >= 30.); // SUCCESS!
     }
 
     if (a < 10)
     {
-        assert(a < 10.); // SUCCESS!
+        assert(a <= 10.); // SUCCESS!
     }
     if (b < 10)
     {
-        assert(b < 10.f); // SUCCESS!
+        assert(b <= 10.f); // SUCCESS!
     }
 
     if (a > 1.)
